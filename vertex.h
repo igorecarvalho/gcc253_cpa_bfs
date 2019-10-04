3241a610fae146479a1b339608fb856a3e97f013
Igor Carvalho <igor.cruz@estudante.ufla.br>
2019-10-04 11:23:46 -0300

    modificacao vertex.h

--- vertex.h
+++ vertex.h
@@ -1,9 +1,10 @@
-#ifndef VERTEX_H
+﻿#ifndef VERTEX_H
 #define VERTEX_H
 
 #include <cstdlib>
 
 //Criação de uma estrutura para comportar cada vertice do grafo
+//teste
 struct Vertex{
 	int vertex;
 	Vertex* left;
