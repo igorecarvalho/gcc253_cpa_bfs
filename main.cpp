6384446cfc7dc3971a1cbfb4f47bb7d98271f4a9
Igor Carvalho <igor.cruz@estudante.ufla.br>
2019-10-08 12:33:22 -0300

    classes implementadas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string>
 #include <set>
+#include <chrono> 
 #include "graph.h"
 
 using namespace std;
@@ -23,6 +24,33 @@ void read_graph(Graph &graph){
 }
 
 int main(int argc, char **argv){
-	
+	if (argc < 2) {
+		cerr << "Correct Usage: ./main <num_vertices> <edges_filename>" << endl;
+		exit(1);
+	}
+
+	Graph graph(atoi(argv[1]));
+	read_graph(graph);
+
+	do{
+		cout << "1 - BFS" << endl;
+		cout << "2 - BAGBFS" << endl;
+		cout << "3 - SAIR" << endl;
+		cout << "Digite a opção desejada!" << endl;
+		cin >> opc;
+
+		auto start = high_resolution_clock::now();
+		switch (opc){
+			case 1: graph.BFS(1);
+				cout << "BFS time: ";
+				break;
+			case 2: graph.BAGBFS(1);
+				cout << "BAGBFS time: ";
+				break;
+		}
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(stop - start);
+		cout << duration.count() << " microseconds" << endl;
+	} while (opc != 3);
 	return 0;
 }
